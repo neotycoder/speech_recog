7796b8d75f51698cbe9671f00c49d6066a2002ee
Ty Lim <neoty888@enterprise.hsd1.ca.comcast.net>
2020-05-23 13:17:11 -0700

    First Commit

---
+++ record_speech.py
@@ -0,0 +1,45 @@
+# Description: This script uses the SpeechRecognition module to record speech
+# and trancribes it to a file
+
+# Import the SpeechRecognition module
+import speech_recognition as sr
+
+class recordSpeech:
+
+    # Setup build the transcribed speech
+    def transcribe(self):
+        
+        # Initialize the speech recognizer module.
+        record = sr.Recognizer()
+        
+        # Begin recording speech
+        mic = sr.Microphone(device_index=0)
+        with mic as source_recording:
+            # Put recording into audio variable
+            audio = record.listen(source_recording)
+        
+        # Return the trancribed speech
+        return record.recognize_google(audio)
+
+    def export(self, transcription):
+
+        # open file to write
+        with open('transcription.txt', mode='w') as file:
+            file.write("Transcribed Speech: ")
+            file.write("\n")
+            file.write(transcription)
+
+        return
+
+def main():
+
+    # Build the object
+    sp = recordSpeech()
+    # Build text to be transcribed. Return it to the text variable
+    text = sp.transcribe()
+    # Perform the export of the speech to a file
+    sp.export(text)
+    
+
+main()
+
